d25262c6663cce9880c6414c0fa0216272fa8586
orazkarl <43617375+orazkarl@users.noreply.github.com>
2020-07-23 22:50:58 +0600

    Initial commit

---
+++ mainpp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainppConfig(AppConfig):
+    name = 'mainpp'
---
+++ mainpp/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+]
